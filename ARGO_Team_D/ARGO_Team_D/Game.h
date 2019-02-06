af01978941262bbb8fea40d96346b9690375c736
C00204864 <C00204864@itcarlow.ie>
2019-02-06 11:48:27 +0000

    Added Demo Box2D code. (TODO: Add static bodies to level).

--- ARGO_Team_D/ARGO_Team_D/Game.h
+++ ARGO_Team_D/ARGO_Team_D/Game.h
@@ -12,6 +12,7 @@
 #include "InputHandler.h"
 #include "Resource Manager/ResourceManager.h"
 #include "ControlSystem.h"
+#include <Box2D\Box2D.h>
 
 using namespace std;
 
@@ -36,8 +37,8 @@ private:
 	SDL_Renderer * m_renderer;
 	bool m_quit = false;
 
-	int m_windowWidth = 1920;
-	int m_windowHeight = 1080;
+	int m_windowWidth = 1280;
+	int m_windowHeight = 720;
 	TTF_Font* Sans;
 
 	RenderSystem m_renderSystem;
@@ -47,8 +48,28 @@ private:
 	ResourceManager* m_resourceManager;
 
 	SDL_Texture * texture;
+	SDL_Texture * square;
 	Mix_Music *	m_testMusic;
 
 	ControlSystem m_controlSystem;
+
+	b2Vec2 m_gravity;
+	b2World m_world;
+
+	// Box2D Test Code
+	float b1X = 400.f;
+	float b1Y = 450.f;
+	float b2X = 400.f;
+	float b2Y = 0.f;
+
+	b2BodyDef m_bodyDef1;
+	b2Body * m_body1;
+	b2PolygonShape m_poly1;
+	b2FixtureDef m_fixture1;
+
+	b2BodyDef m_bodyDef2;
+	b2Body * m_body2;
+	b2PolygonShape m_poly2;
+	b2FixtureDef m_fixture2;
 };
 #endif // !GAME_H
