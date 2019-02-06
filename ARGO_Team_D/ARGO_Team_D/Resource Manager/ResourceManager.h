25930dffef2e09b2074a810aa17f15a9cd50cd3b
Dylaann <dyl.mrph@gmail.com>
2019-02-06 11:26:37 +0000

    Loading from json added

--- ARGO_Team_D/ARGO_Team_D/Resource Manager/ResourceManager.h
+++ ARGO_Team_D/ARGO_Team_D/Resource Manager/ResourceManager.h
@@ -1,16 +1,28 @@
 #ifndef RESOURCEMANAGER_H
 #define RESOURCEMANAGER_H
 
+#include <iostream>
 #include <map>
 #include "ImageResource.h"
 #include "SoundResource.h"
+#include <fstream>
+#include <string>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 
+// Resource Manager class.
+// Creates Resources.
+// Loads paths and id's from json.
+// Stores them in cache.
+// Returns a pointer to the cached asset.
 class ResourceManager
 {
 public:
 	ResourceManager(SDL_Renderer * renderer);
 	~ResourceManager();
+
+	void loadFromJson();
 
 	void addImageResource(ImageResource* resource, const std::string& name, const char * path);
 	void addSoundResource(SoundResource* resource, const std::string& name, const char * path);
@@ -28,6 +40,11 @@ private:
 	std::map<std::string, SoundResource*> m_soundResources;
 
 	SDL_Renderer * m_renderer;
+
+	std::map<std::string, std::string> m_jsonImageFile;
+	std::map<std::string, std::string> m_jsonSoundFile;
+
+	
 };
 
 #endif 
