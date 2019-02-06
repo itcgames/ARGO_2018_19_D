40b1baf402be947fd581bd9e60176ad104778d11
EricOT98 <EricOToole@Outlook.com>
2019-02-06 09:22:45 +0000

    Added render system, sprite component, level

--- ARGO_Team_D/ARGO_Team_D/ECS/Systems/RenderSystem.cpp
+++ ARGO_Team_D/ARGO_Team_D/ECS/Systems/RenderSystem.cpp
@@ -1,6 +1,7 @@
 #include "RenderSystem.h"
 #include <iostream>
 #include "../Components/Components.h"
+#include "../Components/PositionComponent.h"
 #include "../Components/SpriteComponent.h"
 
 /// <summary>
@@ -10,6 +11,7 @@
 void RenderSystem::render(SDL_Renderer* renderer)
 {
 	std::vector<std::string> allowedTypes{ "Position", "Sprite" };
+
 	for (Entity* i : m_entityList) {
 		auto comps = i->getComponentsOfType(allowedTypes);
 		if (comps.size() == allowedTypes.size()) {
@@ -17,6 +19,13 @@ void RenderSystem::render(SDL_Renderer* renderer)
 			std::cout << "Pos: " << p->getPosition().x << ", " << p->getPosition().y << std::endl;
 
 			SpriteComponent * s = dynamic_cast<SpriteComponent*>(comps["Sprite"]);
+			std::cout << std::endl;
+			SdlVector pos = p->getPosition();
+			dest.x = pos.x;
+			dest.y = pos.y;
+			dest.w = s->m_width;
+			dest.h = s->m_height;
+			SDL_RenderCopy(renderer, s->getTexture(), NULL, &dest);
 		}
 	}
 }
