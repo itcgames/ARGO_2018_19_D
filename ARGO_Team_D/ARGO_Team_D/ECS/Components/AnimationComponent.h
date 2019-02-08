5110a9fdbd6e67c54f3cd3beb1af2d3297336e91
EricOT98 <EricOToole@Outlook.com>
2019-02-08 15:20:03 +0000

    Temp fsm commit

--- ARGO_Team_D/ARGO_Team_D/ECS/Components/AnimationComponent.h
+++ ARGO_Team_D/ARGO_Team_D/ECS/Components/AnimationComponent.h
@@ -2,11 +2,18 @@
 #define ANIMATIONCOMPONENT_H
 
 #include "Components.h"
+#include "../Utils/VectorAPI.h"
+
 #include <SDL.h>
+#include <map>
+#include <vector>
 
 class AnimationComponent : public Component {
 public:
 	AnimationComponent() { id = "Animation"; };
+	void addAnimation(const std::string & name, std::vector<VectorAPI> & frames) {
+		m_animations.insert(std::make_pair(name, frames));
+	};
 private:
 	SDL_Texture * m_texture;
 	SDL_Rect m_srcRect;
@@ -15,8 +22,7 @@ private:
 	unsigned short m_currentFrame;
 	float m_animationLength;
 	float m_currentAnimationTime;
-
-
+	std::map<std::string, std::vector<VectorAPI>> m_animations;
 };
 
 #endif //!ANIMATIONCOMPONENT_H
