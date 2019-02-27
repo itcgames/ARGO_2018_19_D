037efab3fb95c8c142651e77b5fa0a3c7f588776
C00204864 <C00204864@itcarlow.ie>
2019-02-27 14:35:15 +0000

    Fixed bug with health not resetting.

--- ARGO_Team_D/ARGO_Team_D/Game.cpp
+++ ARGO_Team_D/ARGO_Team_D/Game.cpp
@@ -496,7 +496,6 @@ void Game::quit()
 
 void Game::setGameState(State state)
 {
-
 	if (m_gameState == State::PlayScreen)
 	{
 		SDL_ShowCursor(SDL_DISABLE);
@@ -657,4 +656,9 @@ void Game::loadAlevel(int num)
 void Game::reloadCurrentlevel()
 {
 	m_levelManager.loadLevel(m_player, *m_resourceManager, m_renderer, m_levelManager.getCurrentLevel());
+}
+
+void Game::resetPlayerHealth()
+{
+	m_healthSystem->reset();
 }
